5270cfcd6577b89d23d5b9844d6637df2883784f
DrewBautista <Andrewbautista319@gmail.com>
2023-08-25 17:55:49 +0800

    try

---
+++ Assets/Script/mainmenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+    public void engineering()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+
+}
